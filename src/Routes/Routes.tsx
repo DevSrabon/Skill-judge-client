3c7c86656f6d1eeb64bfced02823e5d519748878
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2023-02-03 11:36:34 +0300

    top question

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -6,76 +6,72 @@ import Booking from "../pages/Home/Booking/Booking";
 import Login from "../pages/login/Login";
 import Signup from "../pages/login/Signup";
 import PrivateRoute from "./PrivateRoute";
-import TopQuestion from "../pages/Home/TopQuestions/TopQuestion";
+import TopQuestion from "../components/shared/TopQuestion/TopQuestion";
 import QuestionAns from "../pages/QuestionAns/QuestionAns";
 import NotFound from "../pages/404";
 import SingleQnA from "../pages/QuestionAns/SingleQnA";
 import Complier from "../pages/Complier/Complier";
 
-
 export const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Main />,
-		children: [
-			{
-				path: "/",
-				element: <Home />,
-			},
-			{
-				path: "/book/:id",
-				element: (
-					<PrivateRoute>
-						<Booking />
-					</PrivateRoute>
-				),
-				loader: ({ params }) =>
-					fetch(`${process.env.REACT_APP_API_URL}/book/${params.id}`),
-			},
-			{
-				path: "/topquestions/:id",
-				element: (
-					<PrivateRoute>
-						<TopQuestion />
-					</PrivateRoute>
-				),
-				loader: ({ params }) =>
-					fetch(`${process.env.REACT_APP_API_URL}/topquestions/${params.id}`),
-			},
-			{
-				path: "qna",
-				loader: () => fetch("https://skill-judge-server.vercel.app/qna"),
-				element: <QuestionAns></QuestionAns>,
-			},
-			{
-				path: "/login",
-				element: <Login />,
-			},
-			{
-				path: "/signup",
+    {
+        path: "/",
+        element: <Main />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/book/:id",
+                element: (
+                    <PrivateRoute>
+                        <Booking />
+                    </PrivateRoute>
+                ),
+                loader: ({ params }) =>
+                    fetch(`${process.env.REACT_APP_API_URL}/book/${params.id}`),
+            },
+            {
+                path: "/top-questions/:id",
+                element: <TopQuestion />,
+            },
+            {
+                path: "qna",
+                loader: () =>
+                    fetch("https://skill-judge-server.vercel.app/qna"),
+                element: <QuestionAns></QuestionAns>,
+            },
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/signup",
 
-				element: <Signup />,
-			},
-			{
-				path: "/qna",
-				loader: () => fetch(`${process.env.REACT_APP_API_URL}/qna`),
-				element: <QuestionAns></QuestionAns>,
-			},
-			{
-				path: "/singleqna/:id",
-				loader: ({ params }) =>
-					fetch(`${process.env.REACT_APP_API_URL}/qnasingle/${params.id}`),
-				element: <SingleQnA></SingleQnA>,
-			},
-			{
-				path: "/free",
+                element: <Signup />,
+            },
+            {
+                path: "/qna",
+                loader: () => fetch(`${process.env.REACT_APP_API_URL}/qna`),
+                element: <QuestionAns></QuestionAns>,
+            },
+            {
+                path: "/singleqna/:id",
+                loader: ({ params }) =>
+                    fetch(
+                        `${process.env.REACT_APP_API_URL}/qnasingle/${params.id}`
+                    ),
+                element: <SingleQnA></SingleQnA>,
+            },
+            {
+                path: "/free",
 
-				element: <Complier />,
-			},
-		],
-	},
-	{
-		path: "*",
-		element: <NotFound></NotFound>,
-	},
+                element: <Complier />,
+            },
+        ],
+    },
+    {
+        path: "*",
+        element: <NotFound></NotFound>,
+    },
 ]);
--- src/pages/Home/TopQuestions/TopQuestions.tsx
+++ src/pages/Home/TopQuestions/TopQuestions.tsx
@@ -1,59 +1,64 @@
+import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import Spinner from "../../../SharedComponent/Spinner/Spinner";
 
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
-import Spinner from '../../../SharedComponent/Spinner/Spinner';
-import { Link } from 'react-router-dom';
-
-
-interface QuestionType  {
-	title: string;
-	_id: string;
-	details: string;
-};
+interface QuestionType {
+    _id: string;
+    name: string;
+    question: string;
+    info: string;
+    timeCom: string;
+    SpaceCom: string;
+    pseudocode: string[];
+    image: string;
+}
 
 const TopQuestions = () => {
-	const { data = [], isLoading } = useQuery({
-		queryKey: ["topquestions"],
-		queryFn: async () => {
-			const res = fetch(`${process.env.REACT_APP_API_URL}/topquestions`);
-			const data = (await res).json();
-			return data;
-		},
-	});
-	// console.log(data);
-	if (isLoading) {
-		return <Spinner />;
-	}
-	return (
-		<div>
-			<h2 className="text-2xl text-orange-400 md:text-4xl font-bold ">
-				Top Problem Solving Question
-			</h2>
-			<div className="grid grid-cols-1 gap-1 md:gap-10 md:grid-cols-2 lg:grid-cols-3">
-				{data.map((top: QuestionType) => (
-					<>
-						<div className="mt-5 w-full p-5 bg-slate-400 rounded-lg text-white shadow-lg">
-							<h4 className="text-xl font-semibold">{top?.title}</h4>
-							<p className="mt-2 md:mt-5 text-justify">
-								{top?.details.length > 50 ? (
-									<>{top?.details.slice(0, 50) + "....."}</>
-								) : (
-									<>{top?.details}</>
-								)}
-							</p>
-							<div className="text-center md:text-left mt-5">
-								<Link to={`topquestions/${top?._id}`}>
-									<button className="btn btn-sm btn-outline border-slate-100 text-white">
-										See Details
-									</button>
-								</Link>
-							</div>
-						</div>
-					</>
-				))}
-			</div>
-		</div>
-	);
+    const { data = [], isLoading } = useQuery({
+        queryKey: ["top-questions"],
+        queryFn: async () => {
+            const res = await fetch(
+                `${process.env.REACT_APP_API_URL}/top-questions`
+            );
+            const data = await res.json();
+            return data;
+        },
+    });
+    if (isLoading) {
+        return <Spinner />;
+    }
+    return (
+        <div>
+            <h2 className="text-2xl text-orange-400 md:text-4xl font-bold ">
+                Top Problem Solving Question
+            </h2>
+            <div className="grid grid-cols-1 gap-1 md:gap-10 md:grid-cols-2 lg:grid-cols-3">
+                {data.map((top: QuestionType) => (
+                    <>
+                        <div className="mt-5 w-full p-5 bg-slate-400 rounded-lg text-white shadow-lg">
+                            <h4 className="text-xl font-semibold">
+                                {top?.name}
+                            </h4>
+                            <p className="mt-2 md:mt-5 text-justify">
+                                {top?.question.length > 200 ? (
+                                    <>{top?.question.slice(0, 200) + "....."}</>
+                                ) : (
+                                    <>{top?.question}</>
+                                )}
+                            </p>
+                            <div className="text-center md:text-left mt-5">
+                                <Link to={`top-questions/${top?._id}`}>
+                                    <button className="btn btn-sm btn-outline border-slate-100 text-white">
+                                        See Details
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                    </>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default TopQuestions;
