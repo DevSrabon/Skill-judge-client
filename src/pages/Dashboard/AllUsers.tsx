d9eecfd135c38b69c015665da49edf3977578df7
DevSrabon <et.srabon@gmail.com>
2023-02-05 17:29:16 +0600

    fix bugs

--- src/pages/Dashboard/AllUsers.tsx
+++ src/pages/Dashboard/AllUsers.tsx
@@ -11,18 +11,18 @@ const AllUsers = () => {
     const {data: allUsres = []} = useQuery({
         queryKey: ['user'],
         queryFn: async () => {
-           const res = await fetch('http://localhost:5000/all-users')
+           const res = await fetch(`${process.env.REACT_APP_API_URL}/all-users`)
            const data = await res.json()
            return data; 
         }
     })
     
     const handleDeleteUser = (userId: string) => { 
-        fetch(`http://localhost:5000/user/${userId}`,{
-            method: 'DELETE'
-        })
-        .then(res => res.json())
-        .then(data => console.log(data))
+        fetch(`${process.env.REACT_APP_API_URL}/${userId}`, {
+					method: "DELETE",
+				})
+					.then((res) => res.json())
+					.then((data) => console.log(data));
     }
 
     return (
