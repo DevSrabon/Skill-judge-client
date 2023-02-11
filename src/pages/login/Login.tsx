6c4571648a778d2f0f6f5cff7ef36fe8fe3f9890
hachinrayhan <rayhan.hachin@gmail.com>
2023-02-11 08:52:51 +0600

    signup and login loading problem fixed

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -15,7 +15,7 @@ const Login = () => {
 		password: string;
 	};
 
-	const { loginUser, createUserWithGoogle }: any = useContext(AuthContext);
+	const { loginUser, createUserWithGoogle, setLoading }: any = useContext(AuthContext);
 	const [error, setError] = useState('');
 	const { register, formState: { errors }, handleSubmit } = useForm<FormValues>();
 
@@ -46,7 +46,7 @@ const Login = () => {
 			})
 	};
 
-	const handleLogin: SubmitHandler<FormValues> = data => {
+	const handleLogin: SubmitHandler<FormValues> = (data) => {
 		setError('');
 		loginUser(data.email, data.password)
 			.then(result => {
@@ -59,7 +59,9 @@ const Login = () => {
 			})
 			.catch(err => {
 				console.log(err);
-				setError(err.message)
+				toast.error('Email or Password is not correct!');
+				setError(err.message);
+				setLoading(false);
 			})
 	};
 
@@ -74,8 +76,10 @@ const Login = () => {
 				navigate(from, { replace: true });
 			})
 			.catch(err => {
-				console.log(err)
+				console.log(err);
+				toast.error('An Error Occurred');
 				setError(err.message);
+				setLoading(false);
 			})
 	}
 
@@ -88,78 +92,76 @@ const Login = () => {
 					className="w-96"
 					src="https://assets10.lottiefiles.com/packages/lf20_JNfP0LIyMH.json"></Player>
 			</div>
-			<div className="">
-				<div className="w-full max-w-sm mx-auto shadow-xl p-8 rounded-md">
-					<h2 className="text-xl">Login</h2>
-					<form onSubmit={handleSubmit(handleLogin)} className="form-control">
-						{/* Email */}
-						<label className="label">
-							<span className="label-text">Email</span>
-						</label>
-						<input
-							type="email"
-							{...register("email", { required: true })}
-							className="input input-bordered w-full max-w-sm"
-						/>
-						{errors.email?.type === "required" && (
-							<p role="alert" className="text-red-700">
-								Email is required
-							</p>
-						)}
-
-						{/* Password */}
-						<label className="label">
-							<span className="label-text">Password</span>
-						</label>
-						<input
-							type="password"
-							{...register("password", {
-								required: true,
-								minLength: {
-									value: 6,
-									message: "Password must be at least 6 characters",
-								},
-							})}
-							className="input input-bordered w-full max-w-sm"
-						/>
-						<p className="text-red-700">{errors?.password?.message}</p>
-						{errors.password?.type === "required" && (
-							<p role="alert" className="text-red-700">
-								Password is required
-							</p>
-						)}
-
-						{/* Password Reset Button */}
-						<label className="label">
-							<label
-								className="label-text-alt btn btn-xs btn-link no-underline"
-								htmlFor="resetPassword">
-								Forgot Password?
-							</label>
-						</label>
-
-						{/* Submit */}
-						{error && <p className="text-red-700">{error}</p>}
-						<input
-							type="submit"
-							value="Login"
-							className="btn btn-accent w-full max-w-sm"
-						/>
-						<p>
-							New to Skill-Judge?{" "}
-							<Link to={"/signup"} className="text-primary">
-								Create a new account
-							</Link>
+			<div className="w-full max-w-sm mx-auto shadow-xl p-8 rounded-md">
+				<h2 className="text-xl">Login</h2>
+				<form onSubmit={handleSubmit(handleLogin)} className="form-control">
+					{/* Email */}
+					<label className="label">
+						<span className="label-text">Email</span>
+					</label>
+					<input
+						type="email"
+						{...register("email", { required: true })}
+						className="input input-bordered w-full max-w-sm"
+					/>
+					{errors.email?.type === "required" && (
+						<p role="alert" className="text-red-700">
+							Email is required
 						</p>
-						<div className="divider">OR</div>
-					</form>
-					<button
-						onClick={handleGoogleLogin}
-						className="btn btn-outline w-full max-w-sm">
-						Continue With Google
-					</button>
-					<PasswordResetModal></PasswordResetModal>
-				</div>
+					)}
+
+					{/* Password */}
+					<label className="label">
+						<span className="label-text">Password</span>
+					</label>
+					<input
+						type="password"
+						{...register("password", {
+							required: true,
+							minLength: {
+								value: 6,
+								message: "Password must be at least 6 characters",
+							},
+						})}
+						className="input input-bordered w-full max-w-sm"
+					/>
+					<p className="text-red-700">{errors?.password?.message}</p>
+					{errors.password?.type === "required" && (
+						<p role="alert" className="text-red-700">
+							Password is required
+						</p>
+					)}
+
+					{/* Password Reset Button */}
+					<label className="label">
+						<label
+							className="label-text-alt btn btn-xs btn-link no-underline"
+							htmlFor="resetPassword">
+							Forgot Password?
+						</label>
+					</label>
+
+					{/* Submit */}
+					{error && <p className="text-red-700">{error}</p>}
+					<input
+						type="submit"
+						value="Login"
+						className="btn btn-accent w-full max-w-sm"
+					/>
+					<p>
+						New to Skill-Judge?{" "}
+						<Link to={"/signup"} className="text-primary">
+							Create a new account
+						</Link>
+					</p>
+					<div className="divider">OR</div>
+				</form>
+				<button
+					onClick={handleGoogleLogin}
+					className="btn btn-outline w-full max-w-sm">
+					Continue With Google
+				</button>
+				<PasswordResetModal></PasswordResetModal>
 			</div>
 		</div>
 	);
