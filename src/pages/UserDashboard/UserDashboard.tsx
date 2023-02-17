bf467fad85a65605f2a1a32248710833504c5554
DevSrabon <et.srabon@gmail.com>
2023-02-17 16:02:52 +0600

    added icon of dashboad

--- src/pages/UserDashboard/UserDashboard.tsx
+++ src/pages/UserDashboard/UserDashboard.tsx
@@ -1,4 +1,6 @@
 import { Link, Outlet } from 'react-router-dom';
+import { MdQuiz } from "react-icons/md";
+import { BsFillCursorFill } from "react-icons/bs";
 
 const UserDashboard = () => {
 
@@ -15,20 +17,20 @@ const UserDashboard = () => {
 				<div className="drawer-side">
 					<label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
 					<ul className="menu gap-y-2 font-bold p-4 w-80">
-
-								<li className="border rounded-md gap-2">
-									<Link
-										className="bg-emerald-500 focus:bg-black focus:text-white"
-										to={"/userDashboard/quiz"}>
-										Quiz
-									</Link>
-									<Link
-										className="bg-emerald-500 focus:bg-black focus:text-white"
-										to={"/userDashboard/singlesubmission"}>
-										Submission
-									</Link>
-								</li>
-						
+						<li className="border rounded-md gap-2">
+							<Link
+								className="bg-emerald-400 hover:bg-emerald-500 focus:bg-emerald-800 focus:text-white"
+								to={"/userDashboard/quiz"}>
+								<MdQuiz />
+								Quiz Marks
+							</Link>
+							<Link
+								className="bg-emerald-400 hover:bg-emerald-500 focus:bg-emerald-800 focus:text-white"
+								to={"/userDashboard/singlesubmission"}>
+                                    <BsFillCursorFill/>
+								Submission
+							</Link>
+						</li>
 					</ul>
 				</div>
 			</div>
