1f3d27be1d2957be52c3a4175474a4a10ceb1716
DevSrabon <et.srabon@gmail.com>
2023-02-24 13:05:21 +0600

    dashboad data changes

--- src/pages/Dashboard/AllSubmission.tsx
+++ src/pages/Dashboard/AllSubmission.tsx
@@ -1,51 +1,52 @@
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
 
 const AllSubmission = () => {
-        	const { data:submission }: any = useQuery({
-				queryKey: ["allsubmission"],
-				queryFn: () =>
-					fetch(`${process.env.REACT_APP_API_URL}/allsubmission`, {
-						headers: {
-							authorization: `bearer ${localStorage.getItem("token")}`,
-						},
-					}).then((res) => res.json()),
-			});
-			console.log(submission);
-;
-   return (
-			<div className="overflow-x-auto">
-				<table className="table table-zebra w-full">
-					<thead>
-						<tr className="text-center">
-							<th>SL.</th>
-							<th>submission Name</th>
-							<th>Email Address</th>
+	const { data: submission }: any = useQuery({
+		queryKey: ["allsubmission"],
+		queryFn: () =>
+			fetch(`${process.env.REACT_APP_API_URL}/allsubmission`, {
+				headers: {
+					authorization: `bearer ${localStorage.getItem("token")}`,
+				},
+			}).then((res) => res.json()),
+	});
+	React.useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+	return (
+		<div className="overflow-x-auto lg:ml-64 mt-12">
+			<table className="table table-zebra w-full">
+				<thead>
+					<tr className="text-center">
+						<th>SL.</th>
+						<th>submission Name</th>
+						<th>User Name</th>
+						<th>Email Address</th>
 
-							<th>Result</th>
-							
+						<th>Result</th>
+					</tr>
+				</thead>
+				<tbody>
+					{submission?.map((user: any, i: number) => (
+						<tr className="text-center" key={user?._id}>
+							<th>{i + 1}</th>
+							<td>{user?.title}</td>
+							<td>{user?.userName}</td>
+							<td>{user?.email}</td>
+							{user?.correct ? (
+								<>
+									<td>Correct</td>
+								</>
+							) : (
+								<td>Failed</td>
+							)}
 						</tr>
-					</thead>
-					<tbody>
-						{submission?.map((user: any, i: number) => (
-							<tr className="text-center" key={user?._id}>
-								<th>{i + 1}</th>
-								<td>{user?.title}</td>
-                                <td>{user?.email}</td>
-                                {
-                                    user?.correct ? <>
-                                    <td>Correct</td>
-                                    </>: <td>Failed</td>
-                                }
-                                
-							
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</div>
-		
-    );
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 };
 
 export default AllSubmission;
