3c6dc514e04591ac0fa218565fcf2caa19992614
DevSrabon <et.srabon@gmail.com>
2023-02-25 19:42:27 +0600

    fix some error in add-problem

--- src/components/shared/Compiler/components/CodeEditorWindow.tsx
+++ src/components/shared/Compiler/components/CodeEditorWindow.tsx
@@ -2,26 +2,32 @@ import React, { useState } from "react";
 
 import Editor from "@monaco-editor/react";
 
-const CodeEditorWindow = ({ onChange, language, code, theme }) => {
-  const [value, setValue] = useState(code || "");
+const CodeEditorWindow = ({
+	onChange,
+	language,
+	code,
+	theme,
+	valueDefault,
+}) => {
+	const [value, setValue] = useState(code || "");
 
-  const handleEditorChange = (value:any) => {
-    setValue(value);
-    onChange("code", value);
-  };
+	const handleEditorChange = (value: any) => {
+		setValue(value);
+		onChange("code", value);
+	};
 
-  return (
-    <div className="overlay rounded-md overflow-hidden w-full h-full shadow-4xl">
-      <Editor
-        height="75vh"
-        width={`100%`}
-        language={language || "javascript"}
-        value={value}
-        theme={theme}
-        defaultValue="// Code here"
-        onChange={handleEditorChange}
-      />
-    </div>
-  );
+	return (
+		<div className="overlay rounded-md overflow-hidden w-full h-full shadow-4xl">
+			<Editor
+				height="75vh"
+				width={`100%`}
+				language={language || "javascript"}
+				value={value}
+				theme={theme}
+				defaultValue={valueDefault|| "#Code Here"}
+				onChange={handleEditorChange}
+			/>
+		</div>
+	);
 };
 export default CodeEditorWindow;
---
+++ src/pages/Dashboard/ManageProblem.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageProblem = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ManageProblem;
